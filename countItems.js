3f6ed4ffb7252514e3d5e639c5350b870d80c28a
philipyoo <philip.yoo@scality.com>
2019-06-13 16:55:02 -0700

    bf: ZENKO-1736 add scan mongo count items script
    This is mainly to be used by a kubernetes cron job to
    perform the aggregate query scan and update to mongo
    infostore collection.

---
+++ countItems.js
@@ -0,0 +1,38 @@
+const { MongoClientInterface } =
+    require('arsenal').storage.metadata.mongoclient;
+const { Logger } = require('werelogs');
+
+const log = new Logger('S3Utils::ScanItemCount');
+const replicaSetHosts = process.env.MONGODB_REPLICASET;
+
+const config = {
+    replicaSetHosts,
+    writeConcern: 'majority',
+    replicaSet: 'rs0',
+    readPreference: 'primary',
+    database: 'metadata',
+    replicationGroupId: 'RG001',
+    logger: log,
+};
+
+const mongoclient = new MongoClientInterface(config);
+mongoclient.setup(err => {
+    if (err) {
+        log.error('error connecting to mongodb', {
+            error: err,
+        });
+        return process.exit(1);
+    }
+    return mongoclient.scanItemCount(log, error => {
+        // close connection before exiting
+        mongoclient.close();
+        if (error) {
+            log.error('error occurred during mongo scan item count', {
+                error,
+            });
+            return process.exit(1);
+        }
+        log.info('mongo scan item count has completed');
+        return process.exit(0);
+    });
+});
