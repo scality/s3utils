930533c1e5b29646c303ab2fe0411f86847c775d
Artem Bakalov <artem.bakalov@scality.com>
2021-11-03 12:18:21 -0700

    add MultiMap, handlers for repair request events

--- VerifyBucketSproxydKeys/DuplicateKeysWindow.js
+++ VerifyBucketSproxydKeys/DuplicateKeysWindow.js
@@ -1,44 +1,57 @@
 const log = new Logger('s3utils:DuplicateKeysWinow');
 
-class BoundedMap {
+/**
+ * @class
+ * @classdesc sets a maximum window size. Objects inserted when BoundedMap 
+ * is full cause the oldest entry to be deleted.  
+ */
+class BoundedMap extends Map {
     /**
      * @constructor
      * @param {number} maxSize - oldest entries are removed when BoundedMap exceeds this size
      */
-    constructor(maxSize, initialKeys) {
-        this.data = new Map()
+    constructor(maxSize) {
         this.maxSize = maxSize
-        this.iterator = this.data[Symbol.iterator]()
+        this.iterator = super[Symbol.iterator]()
     }
     
     setAndUpdate(key, value) {
-        this.data.set(key, value)
+        super.set(key, value);
+        
         if (this.data.size > this.maxSize) {
-            const oldestKey = this.iterator.next().value[0]
-            data.delete(oldestKey)
-        }
-    }
-
-    get(key) {
-        return this.data.get(key)
-    }
-
-    has(key) {
-        return this.data.has.key()
+            //slide window 
+            const oldestKey = this.iterator.next().value[0];
+            data.delete(oldestKey);
+        };
     }
 }
 
 /**
  * @class
- * @classdesc support data structure to check duplicate sproxyd keys:
- * when a sproxyd key that we found and want to insert already
- * exists in this mapping, it's a duplicate.
+ * @classdesc maps a key to array of values
+ */
+class MultiMap extends Map {
+    set(key, value) {
+        if (!super.get(key)) {
+            super.set(key, [value]);
+        } else {
+            const current = super.get(key);
+            current.append(value);
+            super.set(key, current);
+        };
+    }
+}
+
+/**
+ * @class
+ * @classdesc support data structure to check sproxyd keys
+ * and handle any needed repairs in near real-time
  */
  class SproxydKeys {
      /**
      * @constructor
      * @param {number} windowSize - maximum number of sproxyd keys to track
-     * @param {Map { <string>: Array<Function>} } subscribersMap - Map of events to functions that listen for and respond to SproxydKeys events 
+     * @param { MultiMap } subscribers - Map of events to handlers that listen for and respond to SproxydKeys events 
      */
     constructor(windowSize, subscribers) {
         // mapping schema:
@@ -48,24 +61,23 @@ class BoundedMap {
         //     "sproxydKey3": "s3://bucket1/masterKey2"
         // }
         this.sproxydKeys = new BoundedMap(windowSize);
-        this.subscribersMap = subscribersMap;
+        this.subscribers = subscribers;
     }
 
     checkDuplicate(key) {
         const objectId2 = this.sproxydKeys.get(key);
             if (objectId2) {
                 // duplicate found
-                dupInfo = { objectId: objectId2, key };
+                const dupInfo = { objectId: objectId2, key };
                 
-                if (dupInfo) {
-                    // log.error('duplicate sproxyd key found', {
-                    //     objectUrl: objectId,
-                    //     objectUrl2: dupInfo.objectId,
-                    //     sproxydKey: dupInfo.key,
-                    // });
-
-                    //TODO: pass directly into repairDuplicateVersions:repairObject bypassing logging/reading from log
-                }
+                // log.error('duplicate sproxyd key found', {
+                //     objectUrl: objectId,
+                //     objectUrl2: dupInfo.objectId,
+                //     sproxydKey: dupInfo.key,
+                // });
+                subscribers['duplicateSproxyKeyFound'].forEach(handler => handler(dupInfo));
+                //TODO: pass directly into repairDuplicateVersions:repairObject bypassing logging/reading from log
+                
             } else {
                 this.sproxydKeys.setAndUpdate(key, objectId);
             }
