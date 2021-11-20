94346e25bd36f5299c8e259354c2da979d67f109
Artem Bakalov <artem.bakalov@scality.com>
2021-11-19 18:07:06 -0800

    jsDoc for window classes

--- SproxydKeysScan/DuplicateKeysWindow.js
+++ SproxydKeysScan/DuplicateKeysWindow.js
@@ -3,7 +3,7 @@ const log = new Logger('s3utils:DuplicateKeysWinow');
 
 /**
  * @class
- * @classdesc sets a maximum window size. Objects inserted when BoundedMap
+ * @classdesc Sets a maximum window size for a Map. Objects inserted when BoundedMap
  * is full cause the oldest entry to be deleted.
  */
 class BoundedMap extends Map {
@@ -17,6 +17,12 @@ class BoundedMap extends Map {
         this.iterator = super[Symbol.iterator]();
     }
 
+    /**
+     * Sets new item in Map, and deletes oldest if Map.size > maxSize
+     * @param {string} key - Map key.
+     * @param {any} value - Value associated to key.
+     * @returns {(string|null)} - Removed key or null if no key was removed.
+     */
     setAndUpdate(key, value) {
         super.set(key, value);
 
@@ -36,6 +42,13 @@ class BoundedMap extends Map {
  * @classdesc maps a key to array of values
  */
 class MultiMap extends Map {
+    /**
+     * Sets a value to a list of values that share the same key.
+     * @example - { key: [v1, v2, v3.. ]}
+     * @param {string} key - Map key.
+     * @param {any} value - Value associated to key.
+     * @returns {undefined}
+     */
     set(key, value) {
         if (!super.get(key)) {
             super.set(key, [value]);
@@ -63,7 +76,13 @@ class SproxydKeysProcessor {
         this.sproxydKeys = new BoundedMap(windowSize);
         this.subscribers = subscribers;
     }
-
+    /**
+     * If a duplicate Sproxyd key is found, a handler is called to generate new keys.
+     * @param {string} sproxydKey - sproxyd Key.
+     * @param {string} objectKey - Versioned object Key.
+     * @param {string} bucket - Bucket name.
+     * @returns {undefined}
+     */
     checkDuplicate(sproxydKey, objectKey, bucket) {
         const existingObjectKey = this.sproxydKeys.get(sproxydKey);
         if (existingObjectKey && existingObjectKey !== objectKey) {
@@ -90,6 +109,13 @@ class SproxydKeysProcessor {
         }
     }
 
+    /**
+     * Checks if a duplicate Exists for each Sproxyd Key.
+     * @param {string} objectKey - Object Key.
+     * @param {Array.<string>} sproxydKeys - Array of Sproxyd Keys for associated Object key.
+     * @param {string} bucket - bucket name.
+     * @returns {undefined}
+     */
     insert(objectKey, sproxydKeys, bucket) {
         for (const sproxydKey of sproxydKeys) {
             this.checkDuplicate(sproxydKey, objectKey, bucket);
