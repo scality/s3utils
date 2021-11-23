c317da62c06e62466b342ba97a22353a7fd79878
Artem Bakalov <artem.bakalov@scality.com>
2021-11-22 17:12:03 -0800

    use process.nextTick`

--- SproxydKeysScan/DuplicateKeysIngestion.js
+++ SproxydKeysScan/DuplicateKeysIngestion.js
@@ -63,7 +63,7 @@ class RaftJournalReader {
      */
     setBegin(cb) {
         if (this.begin) {
-            return cb();
+            return process.nextTick(cb);
         }
         // fetch one record to get cseq
         const requestUrl = `${this.url}/log?begin=1&limit=1&targetLeader=False`;
