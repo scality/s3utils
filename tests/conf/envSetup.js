32400d04b0a2fb490019c612ef7501a21fd9b2c7
williamlardier <william.lardier@scality.com>
2023-10-26 10:46:12 +0200

    S3UTILS-148: fix test framework issue

--- tests/conf/envSetup.js
+++ tests/conf/envSetup.js
@@ -1,2 +1,7 @@
+const { TextEncoder, TextDecoder } = require('util');
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
 process.env.BUCKETD_HOSTPORT = 'localhost:9000';
 process.env.SPROXYD_HOSTPORT = 'localhost';
