938a85d790fa62250fd89ca1dd7f716d9602a3ec
williamlardier <william.lardier@scality.com>
2024-03-25 16:34:57 +0100

    S3UTILS-155: support new fields when consolidating the metrics
    We introduce new fields:
    
    - (non)CurrentCold fields are counting the objects that have
      archive set in their metadata, i.e., in cold storage.
    - restoring tracks objects being restored.
    - restored tracks objects that are restored, and not yet expired.

--- CountItems/utils/utils.js
+++ CountItems/utils/utils.js
@@ -11,6 +11,10 @@ function consolidateDataMetrics(target, source) {
             usedCapacity: {
                 current: 0,
                 nonCurrent: 0,
+                restored: 0,
+                restoring: 0,
+                currentCold: 0,
+                nonCurrentCold: 0,
             },
         });
     }
@@ -19,6 +23,10 @@ function consolidateDataMetrics(target, source) {
             objectCount: {
                 current: 0,
                 nonCurrent: 0,
+                currentCold: 0,
+                nonCurrentCold: 0,
+                restored: 0,
+                restoring: 0,
                 deleteMarker: 0,
             },
         });
@@ -29,9 +37,18 @@ function consolidateDataMetrics(target, source) {
     const { usedCapacity, objectCount } = source;
     resTarget.usedCapacity.current += usedCapacity && usedCapacity.current ? usedCapacity.current : 0;
     resTarget.usedCapacity.nonCurrent += usedCapacity && usedCapacity.nonCurrent ? usedCapacity.nonCurrent : 0;
+    resTarget.usedCapacity.currentCold += usedCapacity && usedCapacity.currentCold ? usedCapacity.currentCold : 0;
+    resTarget.usedCapacity.nonCurrentCold += usedCapacity && usedCapacity.nonCurrentCold ? usedCapacity.nonCurrentCold : 0;
+    resTarget.usedCapacity.restoring += usedCapacity && usedCapacity.restoring ? usedCapacity.restoring : 0;
+    resTarget.usedCapacity.restored += usedCapacity && usedCapacity.restored ? usedCapacity.restored : 0;
+
     resTarget.objectCount.current += objectCount && objectCount.current ? objectCount.current : 0;
     resTarget.objectCount.nonCurrent += objectCount && objectCount.nonCurrent ? objectCount.nonCurrent : 0;
     resTarget.objectCount.deleteMarker += objectCount && objectCount.deleteMarker ? objectCount.deleteMarker : 0;
+    resTarget.objectCount.currentCold += objectCount && objectCount.currentCold ? objectCount.currentCold : 0;
+    resTarget.objectCount.nonCurrentCold += objectCount && objectCount.nonCurrentCold ? objectCount.nonCurrentCold : 0;
+    resTarget.objectCount.restoring += objectCount && objectCount.restoring ? objectCount.restoring : 0;
+    resTarget.objectCount.restored += objectCount && objectCount.restored ? objectCount.restored : 0;
     return resTarget;
 }
 
