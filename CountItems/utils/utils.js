91d52df40f6993ae174d88ae1ee3e3c71e605617
williamlardier <william.lardier@scality.com>
2024-03-26 17:09:45 +0100

    S3UTILS-155: keep the original values for current & nonCurrent
    Thsi ensures backward compatibility and remove the need to update
    the services using theses values: ZKUI and PENSIVE-API

--- CountItems/utils/utils.js
+++ CountItems/utils/utils.js
@@ -11,12 +11,12 @@ function consolidateDataMetrics(target, source) {
             usedCapacity: {
                 current: 0,
                 nonCurrent: 0,
-                currentCold: 0,
-                nonCurrentCold: 0,
-                currentRestored: 0,
-                currentRestoring: 0,
-                nonCurrentRestored: 0,
-                nonCurrentRestoring: 0,
+                _currentCold: 0,
+                _nonCurrentCold: 0,
+                _currentRestored: 0,
+                _currentRestoring: 0,
+                _nonCurrentRestored: 0,
+                _nonCurrentRestoring: 0,
             },
         });
     }
@@ -25,12 +25,12 @@ function consolidateDataMetrics(target, source) {
             objectCount: {
                 current: 0,
                 nonCurrent: 0,
-                currentCold: 0,
-                nonCurrentCold: 0,
-                currentRestored: 0,
-                currentRestoring: 0,
-                nonCurrentRestored: 0,
-                nonCurrentRestoring: 0,
+                _currentCold: 0,
+                _nonCurrentCold: 0,
+                _currentRestored: 0,
+                _currentRestoring: 0,
+                _nonCurrentRestored: 0,
+                _nonCurrentRestoring: 0,
                 deleteMarker: 0,
             },
         });
@@ -41,22 +41,33 @@ function consolidateDataMetrics(target, source) {
     const { usedCapacity, objectCount } = source;
     resTarget.usedCapacity.current += usedCapacity && usedCapacity.current ? usedCapacity.current : 0;
     resTarget.usedCapacity.nonCurrent += usedCapacity && usedCapacity.nonCurrent ? usedCapacity.nonCurrent : 0;
-    resTarget.usedCapacity.currentCold += usedCapacity && usedCapacity.currentCold ? usedCapacity.currentCold : 0;
-    resTarget.usedCapacity.nonCurrentCold += usedCapacity && usedCapacity.nonCurrentCold ? usedCapacity.nonCurrentCold : 0;
-    resTarget.usedCapacity.currentRestoring += usedCapacity && usedCapacity.currentRestoring ? usedCapacity.currentRestoring : 0;
-    resTarget.usedCapacity.currentRestored += usedCapacity && usedCapacity.currentRestored ? usedCapacity.currentRestored : 0;
-    resTarget.usedCapacity.nonCurrentRestoring += usedCapacity && usedCapacity.nonCurrentRestoring ? usedCapacity.nonCurrentRestoring : 0;
-    resTarget.usedCapacity.nonCurrentRestored += usedCapacity && usedCapacity.nonCurrentRestored ? usedCapacity.nonCurrentRestored : 0;
+    resTarget.usedCapacity._currentCold += usedCapacity && usedCapacity._currentCold ? usedCapacity._currentCold : 0;
+    resTarget.usedCapacity._nonCurrentCold += usedCapacity && usedCapacity._nonCurrentCold ? usedCapacity._nonCurrentCold : 0;
+    resTarget.usedCapacity._currentRestoring += usedCapacity && usedCapacity._currentRestoring ? usedCapacity._currentRestoring : 0;
+    resTarget.usedCapacity._currentRestored += usedCapacity && usedCapacity._currentRestored ? usedCapacity._currentRestored : 0;
+    resTarget.usedCapacity._nonCurrentRestoring += usedCapacity && usedCapacity._nonCurrentRestoring ? usedCapacity._nonCurrentRestoring : 0;
+    resTarget.usedCapacity._nonCurrentRestored += usedCapacity && usedCapacity._nonCurrentRestored ? usedCapacity._nonCurrentRestored : 0;
 
     resTarget.objectCount.current += objectCount && objectCount.current ? objectCount.current : 0;
     resTarget.objectCount.nonCurrent += objectCount && objectCount.nonCurrent ? objectCount.nonCurrent : 0;
     resTarget.objectCount.deleteMarker += objectCount && objectCount.deleteMarker ? objectCount.deleteMarker : 0;
-    resTarget.objectCount.currentCold += objectCount && objectCount.currentCold ? objectCount.currentCold : 0;
-    resTarget.objectCount.nonCurrentCold += objectCount && objectCount.nonCurrentCold ? objectCount.nonCurrentCold : 0;
-    resTarget.objectCount.currentRestoring += objectCount && objectCount.currentRestoring ? objectCount.currentRestoring : 0;
-    resTarget.objectCount.currentRestored += objectCount && objectCount.currentRestored ? objectCount.currentRestored : 0;
-    resTarget.objectCount.nonCurrentRestoring += objectCount && objectCount.nonCurrentRestoring ? objectCount.nonCurrentRestoring : 0;
-    resTarget.objectCount.nonCurrentRestored += objectCount && objectCount.nonCurrentRestored ? objectCount.nonCurrentRestored : 0;
+    resTarget.objectCount._currentCold += objectCount && objectCount._currentCold ? objectCount._currentCold : 0;
+    resTarget.objectCount._nonCurrentCold += objectCount && objectCount._nonCurrentCold ? objectCount._nonCurrentCold : 0;
+    resTarget.objectCount._currentRestoring += objectCount && objectCount._currentRestoring ? objectCount._currentRestoring : 0;
+    resTarget.objectCount._currentRestored += objectCount && objectCount._currentRestored ? objectCount._currentRestored : 0;
+    resTarget.objectCount._nonCurrentRestoring += objectCount && objectCount._nonCurrentRestoring ? objectCount._nonCurrentRestoring : 0;
+    resTarget.objectCount._nonCurrentRestored += objectCount && objectCount._nonCurrentRestored ? objectCount._nonCurrentRestored : 0;
+
+    // Current and NonCurrent are the total of all other metrics
+    resTarget.usedCapacity.current += usedCapacity
+        ? usedCapacity._currentCold + usedCapacity._currentRestored + usedCapacity._currentRestoring : 0;
+    resTarget.usedCapacity.nonCurrent += usedCapacity
+        ? usedCapacity._nonCurrentCold + usedCapacity._nonCurrentRestored + usedCapacity._nonCurrentRestoring : 0;
+    resTarget.objectCount.current += objectCount
+        ? objectCount._currentCold + objectCount._currentRestored + objectCount._currentRestoring : 0;
+    resTarget.objectCount.nonCurrent += objectCount
+        ? objectCount._nonCurrentCold + objectCount._nonCurrentRestored + objectCount._nonCurrentRestoring : 0;
+
     return resTarget;
 }
 
