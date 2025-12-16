09146dd2a92c433c00c927dc0b3adcf7aa7e84e4
Thomas Carmet <8408330+tcarmet@users.noreply.github.com>
2025-12-16 18:38:26 +0000

    S3UTILS-210: include all files in coverage report

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,12 @@ module.exports = {
     moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
     setupFiles: ['<rootDir>/tests/conf/envSetup.js'],
     testMatch: ['**/tests/**/*.js?(x)'],
+    collectCoverageFrom: [
+        '**/*.js',
+        '!**/node_modules/**',
+        '!**/tests/**',
+        '!**/coverage/**',
+        '!jest.config.js',
+    ],
 };
 
